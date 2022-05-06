50c10a373a1eabd3e571ad87695e7fc5039a3bb8
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2022-05-06 13:01:19 -0300

    configured container app to make use of marketing app

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MarketingApp from './components/MarketingApp';
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
