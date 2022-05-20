dabf1d43d6d7c3a0d88c1b178c8e8723a038b8f9
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2022-05-20 11:18:10 -0300

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export const App = () => {
   return (
     <div>
-      <h1>Test</h1>
+      <h1>Header</h1>
       <hr />
       <MarketingApp />
     </div>
