b3a67f6075c62a763f8abf06a8b9e595653831fc
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2022-05-20 12:51:06 -0300

    add production prefix to styles provider

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'cont',
+});
 
 export const App = () => {
   return (
-    <div>
-      <h1>Header</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,18 @@
-import { StylesProvider } from '@material-ui/core';
 import React from 'react';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'mkt',
+});
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
